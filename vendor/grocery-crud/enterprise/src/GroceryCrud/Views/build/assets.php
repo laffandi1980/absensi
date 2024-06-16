b33bd59655014e66bf5011a12feb3a6b1c69631a
laffandi1980 <dianaffandi@gmail.com>
2024-06-16 16:09:59 +0700

    upload

---
+++ vendor/grocery-crud/enterprise/src/GroceryCrud/Views/build/assets.php
@@ -0,0 +1,5 @@
+<?php /** Auto generated file, please do not edit **/
+
+$this->setJavaScriptFile($this->_assetsFolder . 'static/js/bundle.v3.1.3.78805020.js');
+$this->setJavaScriptFile($this->_assetsFolder . 'static/js/97.3d165f00.chunk.js');
+$this->setJavaScriptFile($this->_assetsFolder . 'static/js/main.365305a8.chunk.js');
